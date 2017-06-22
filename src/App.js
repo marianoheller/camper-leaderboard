650bdd7b1c17f41f785db013bc91e506393ede6b
Mariano Mariano <Mariano Mariano>
2017-06-22 07:46:57 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Config } from './config'
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <p>asdasd</p>
+    </div>
+    );
+  }
+}
+
---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+
+export const Config = {
+    urls: {
+        last30 : "https://fcctop100.herokuapp.com/api/fccusers/top/recent",
+        allTime: "https://fcctop100.herokuapp.com/api/fccusers/top/alltime",
+    },
+}
